30971a41c5b587cdb85f6566c46ec9ffa12ab3bc
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-04-08 00:24:35 -0300

    create submenu
    create submenu and fix responsive width

--- projetonext/devmotors/src/app/page.tsx
+++ projetonext/devmotors/src/app/page.tsx
@@ -1,19 +1,10 @@
+import { SubMenu } from "@/components/home/submenu";
+
 
 export default function Home() {
   return (
-      <main>
-        <div style={{ width: "100%", height: 600, backgroundColor: "#121212", marginBottom: 24}}>
-        <h1>teste</h1>
-        </div>
-
-        <div style={{ width: "100%", height: 600, backgroundColor: "blue", marginBottom: 24}}>
-        <h1>teste</h1>
-        </div>
-
-        <div style={{ width: "100%", height: 600, backgroundColor: "green", marginBottom: 24}}>
-        <h1>teste</h1>
-        </div>
-     
-     
-      </main>
-  )}
+    <main>
+      <SubMenu/>
+    </main>
+  )
+}
