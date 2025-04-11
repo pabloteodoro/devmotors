14df04941957b0afb3958eb949756b1f0fd073ee
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-04-10 23:22:01 -0300

    Add new style for components

--- projetonext/devmotors/src/app/page.tsx
+++ projetonext/devmotors/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { SubMenu } from "@/components/home/submenu";
 import { getDataHome } from '@/utils/actions/get-data'
 import { HomeProps } from '@/utils/actions/home.type'
+import { Hero } from '@/components/hero'
+import { Phone } from 'lucide-react'
 
 
 export default async function Home() {
@@ -9,6 +11,17 @@ export default async function Home() {
   return (
     <main>
       <SubMenu/>
+
+      <Hero
+      heading={object.metadata.heading}
+      buttonTitle={object.metadata.cta_button.title}
+      buttonUrl={object.metadata.cta_button.url}
+      bannerUrl={object.metadata.banner.url}
+      icon={<Phone size={24} color="#fff" />}      
+      
+      
+      
+      />
     </main>
-  )
+  );
 }
