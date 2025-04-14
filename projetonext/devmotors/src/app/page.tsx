d54b9616312014fe3dc3f1ae360b72c17b083f37
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-04-14 19:05:01 -0300

    New feature, add a page contacts

--- projetonext/devmotors/src/app/page.tsx
+++ projetonext/devmotors/src/app/page.tsx
@@ -5,6 +5,7 @@ import { Hero } from '@/components/hero'
 import { Phone } from 'lucide-react'
 import { Services } from "@/components/home/services";
 import { Container } from "@/components/container";
+import { Footer } from "@/components/home/footer";
 
 export default async function Home() {
   const { object }: HomeProps = await getDataHome();
@@ -25,6 +26,7 @@ export default async function Home() {
       />
       <Container>
       <Services object={object}/>
+      <Footer object={object}/>
       </Container>
      
     </main>
