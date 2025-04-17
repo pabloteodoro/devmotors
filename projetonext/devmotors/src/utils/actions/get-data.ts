07711cb8d7d7a073f21ada398a55e6eb404ec979
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-04-16 22:25:16 -0300

    Update get-data.ts

--- projetonext/devmotors/src/utils/actions/get-data.ts
+++ projetonext/devmotors/src/utils/actions/get-data.ts
@@ -13,3 +13,18 @@ export async function getDataHome() {
         throw new Error("Failed to fetch data");
     }
 }
+
+export async function getSubMenu() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`)
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch data");
+        }
+
+        return res.json();
+
+    } catch(err) {
+        throw new Error("Failed to fetch data");
+    }
+}
